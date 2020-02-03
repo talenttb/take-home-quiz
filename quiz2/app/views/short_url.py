798fc6ae6e65badebdc47aae9af3fbc1a3178b1c
Liyu Liu <liyu.liu@access-company.com>
2020-02-03 13:02:27 +0800

    add logs

--- quiz2/app/views/short_url.py
+++ quiz2/app/views/short_url.py
@@ -1,16 +1,19 @@
 # import json
 # import yaml
 # from app import log
-from flask import Blueprint
-from flask import jsonify
-# , current_app as app
-# from app.database import db
-# from app.view_model import ma
-# from flask_apispec import use_kwargs, marshal_with
+import logging
+from flask import Blueprint, jsonify
 
 short_url = Blueprint('short_url', __name__)
+logger = logging.getLogger(__name__)
+
+
+def record():
+    logger.info('logged by logging module of functions')
+    return True
 
 
 @short_url.route('/')
 def short_url_root():
+    logger.info('in short url root')
     return jsonify({'test': 'ok'})
--- quiz2/run.py
+++ quiz2/run.py
@@ -1,7 +1,29 @@
-from app import create_app
+# -*- coding: utf-8 -*-
+# # from app import create_app
 
-app = create_app()
+# app = create_app()
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
+
+from app import create_app
+import logging
+from flask_script import Manager, Shell, Server
+
+server = Server(host="0.0.0.0", threaded=True)
+app = create_app('default')
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("server", server)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    manager.run()
