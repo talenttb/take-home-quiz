753b65dd9d6d44e3e3b722223a49b2ed12a200ab
Liyu Liu <liyu.liu@access-company.com>
2020-02-04 13:23:55 +0800

    add answer of quiz1

--- quiz1/main.py
+++ quiz1/main.py
@@ -11,8 +11,9 @@ def count_digit_v1(n):
     return res
 
 
-print(count_digit_v1(120))
+# print(count_digit_v1(120))
 # print(count_digit_v1(10))
 # print(count_digit_v1(50))
 # print(count_digit_v1(70))
 # print(count_digit_v1(80))
+print(count_digit_v1(1000))
